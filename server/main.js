9108475e036df13a249d0b1a47709d9e56bcc895
PorYoung <poryoung@outlook.com>
2023-08-29 00:43:51 +0800

    增加Electron配置

--- server/main.js
+++ server/main.js
@@ -1,55 +1,90 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const server = app.listen(3000, function () {
-	console.log("server running on port 3000");
-});
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
+const { app, BrowserWindow, screen, webFrame } = require("electron");
+const { spawn } = require("child_process");
+const path = require("path");
+
+const appName = app.getPath("exe");
+const windowConfig = {
+	productName: "NotesTick",
+	appId: "NotesTick",
+	title: "NotesTick",
+	fullscreenable: true,
+	fullscreen: false,
+	autoHideMenuBar: true,
+	width: 1920,
+	height: 1080,
+	show: false,
+	icon: path.join(__dirname, "./static/images/favicon.ico"),
+	webPreferences: {
+		//解决跨域问题
+		webSecurity: false,
 	},
-});
-const solo = require("./app/solo");
+};
+const expressPath = path.join(__dirname, "./server.js");
+const expressUrl = "http://localhost:3000/app";
 
-//跨域问题解决方面
-const cors = require("cors");
-app.use(cors());
+let mainWindow;
 
-Set.prototype.isDuplicate = (obj, key) => {
-	for (const item of mySet) {
-		if (item[key] === obj[key]) {
-			return true;
-		}
-	}
-	return false;
+const stripAnsiColors = (text) => {
+	return text.replace(
+		/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+		""
+	);
 };
 
-Set.prototype.deleteByKey = (key, val) => {
-	for (const item of mySet) {
-		if (item[key] === val) {
-			mySet.delete(item);
-			break;
-		}
-	}
+const redirectOutput = (stream) => {
+	stream.on("data", (data) => {
+		data.toString()
+			.split("\n")
+			.forEach((line) => {
+				if (line !== "") {
+					mainWindow.webContents.send(
+						"server-log-entry",
+						stripAnsiColors(line)
+					);
+				}
+			});
+	});
 };
 
-// 静态文件服务
-app.use("/static", express.static("static"));
-app.use(express.static("public", { index: "index.html" }));
+function createWindow() {
+	const expressAppProcess = spawn(appName, [expressPath], {
+		env: { ELECTRON_RUN_AS_NODE: "1" },
+	});
 
-// 连接前检查
-io.use((socket, next) => {
-	solo.preInterceptor(socket, next);
+	[expressAppProcess.stdout, expressAppProcess.stderr].forEach(
+		redirectOutput
+	);
+
+	const size = screen.getPrimaryDisplay().workAreaSize;
+	const width = size.width;
+	if (width >= 1920) {
+		mainWindow = new BrowserWindow(windowConfig);
+	} else {
+		let height = parseInt((1080 * size.width) / 1920 + 30);
+		mainWindow = new BrowserWindow(
+			Object.assign({}, windowConfig, { width: width, height: height })
+		);
+	}
+
+	mainWindow.maximize();
+
+	mainWindow.loadURL(expressUrl);
+
+	mainWindow.on("closed", function () {
+		mainWindow = null;
+	});
+
+	mainWindow.once("ready-to-show", () => {
+		mainWindow.show();
+	});
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+	app.quit();
 });
-
-// 客户端连接事件
-io.on("connection", (socket) => {
-	console.log("A user connected", socket.id);
-	const { name, mode, room } = socket.handshake.query;
-
-	solo.createSoloEnv(io, socket);
+app.on("activate", () => {
+	if (win == null) {
+		createWindow();
+	}
 });
-
-// HTTP Server
-const midiRouter = require("./app/midi");
-app.use("/api", midiRouter);
